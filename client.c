06e1391e620590ec28f6d7eac5566ad88f144a4b
Uday Sharma <itsudayy@gmail.com>
2024-08-04 01:53:46 +0530

    project done

---
+++ client.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[255];
+
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) 
+        fprintf(stderr, "ERROR opening socket");
+        
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr, "ERROR no such host");
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("Connection Failed");
+
+    while(1) {
+        bzero(buffer, 255);
+        printf("Client: ");
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+
+        if (n < 0)
+            error("ERROR on writing");
+
+        bzero(buffer, 255);
+        n = read(sockfd, buffer, 255);
+        if (n < 0) 
+            error("ERROR on reading");
+        
+        printf("Server: %s", buffer);
+
+        int i = strncmp("Bye", buffer, 3);
+        if (i == 0)
+            break;
+    }
+
+    close(sockfd);
+    return 0;
+}
